d21ea1111f81b5894c22dfc8c8845e535511f677
dragonmux <git@dragonmux.network>
2026-04-18 04:35:35 +0100

    lattice_isp_clock: Defined the "Flash" (E²CMOS NVM) for the ispCLOCK and stubs for manipulating it

--- src/target/lattice_isp_clock.c
+++ src/target/lattice_isp_clock.c
@@ -38,9 +38,82 @@
 #include "target_internal.h"
 #include "lattice_isp_clock.h"
 
+/* Instructions for the ispCLOCK that we care about */
+#define IR_ADDRESS_SHIFT    0x01U
+#define IR_DATA_SHIFT       0x02U
+#define IR_BULK_ERASE       0x03U
+#define IR_PROGRAM          0x07U
+#define IR_PROGRAM_SECURITY 0x09U
+#define IR_VERIFY           0x0aU
+#define IR_DISCHARGE        0x14U
+#define IR_PROGRAM_ENABLE   0x15U
+#define IR_USERCODE         0x17U
+#define IR_PROGRAM_USERCODE 0x1aU
+#define IR_PROGRAM_DISABLE  0x1eU
+#define IR_ERASE_DONE       0x24U
+#define IR_PROGRAM_INCR     0x27U
+#define IR_VERIFY_INCR      0x2aU
+#define IR_PROGRAM_DONE     0x2fU
+
+#define ISP_CLOCK_NVM_BASE 0x00000000U
+#define ISP_CLOCK_NVM_SIZE 0x00001500U
+
+static bool isp_clock_enter_flash_mode(target_s *target);
+static bool isp_clock_exit_flash_mode(target_s *target);
+static bool isp_clock_flash_write(target_flash_s *flash, target_addr_t dest, const void *buffer, size_t lenth);
+static bool isp_clock_flash_mass_erase(target_flash_s *flash, platform_timeout_s *print_progress);
+
+static void isp_clock_add_flash(target_s *const target)
+{
+	target_flash_s *flash = calloc(1, sizeof(*flash));
+	if (!flash) { /* calloc failed: heap exhaustion */
+		DEBUG_ERROR("calloc: failed in %s\n", __func__);
+		return;
+	}
+
+	/*
+	 * The ispCLOCK device has a small NVM that we treat like it's Flash here.
+	 * It's arranged in 42-bit blocks ("columns") with a 10-bit address - this gives
+	 * a total of 5376 bytes of "E²CMOS" NVM.
+	 */
+	flash->start = ISP_CLOCK_NVM_BASE;
+	flash->length = ISP_CLOCK_NVM_SIZE;
+	flash->blocksize = ISP_CLOCK_NVM_SIZE;
+	/* Have to write 4 blocks at a time to get an integer number of bytes */
+	flash->writesize = 21U;
+	flash->mass_erase = isp_clock_flash_mass_erase;
+	flash->write = isp_clock_flash_write;
+	flash->erased = 0xffU;
+	target_add_flash(target, flash);
+}
+
 void lattice_isp_clock_handler(const uint8_t dev_index)
 {
 	target_s *target = target_new();
 	target->driver = "Lattice";
 	target->core = "ispCLOCK";
+
+	target->enter_flash_mode = isp_clock_enter_flash_mode;
+	target->exit_flash_mode = isp_clock_exit_flash_mode;
+	isp_clock_add_flash(target);
+}
+
+bool isp_clock_enter_flash_mode(target_s *const target)
+{
+	return true;
+}
+
+bool isp_clock_exit_flash_mode(target_s *const target)
+{
+	return true;
+}
+
+static bool isp_clock_flash_write(target_flash_s *flash, target_addr_t dest, const void *buffer, size_t length)
+{
+	return true;
+}
+
+static bool isp_clock_flash_mass_erase(target_flash_s *flash, platform_timeout_s *print_progress)
+{
+	return true;
 }
